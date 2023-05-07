c425a0b06d7807d675a2e51d85db8374cd295b53
Pham Duong <phamquangduong2002@gmail.com>
2023-05-07 18:25:00 +0700

    #865c6bn53 add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins'],
+      },
+      colors: {
+        'white-1': '#FCFCFF',
+        'blue-1': '#006AD0',
+        'black-1': '#3f3f3f',
+        'gray-1': '#e5e5e5',
+      },
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      transitionDuration: {
+        400: '400ms',
+      },
+      animation: {
+        appear: 'appear .4s ease-in-out',
+      },
+      screens: {
+        lg: '992px',
+        xl: '1200px',
+      },
+    },
   },
   plugins: [],
 };
