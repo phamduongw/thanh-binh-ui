8c7bb0ce5cff81a9382c33106a7e0e461f2036dd
Pham Duong <phamquangduong2002@gmail.com>
2023-09-24 20:15:06 +0700

    Feature: Blog page addition (#8)

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,11 +1,30 @@
 import { Helmet } from 'react-helmet-async';
 
+import BlogList from '~/components/BlogList';
+import CallToAction from '~/components/CallToAction';
+import Paragraph from '~/components/Paragraph';
+
+import BLOGS from '~/data/BLOGS';
+
 const Blog = ({ title }) => (
   <main>
     <Helmet>
       <title>{title}</title>
     </Helmet>
-    Blog
+    <section className="thin-padding">
+      <div className="section-container-flex align-center">
+        <div className="section-heading text-center">
+          <div className="section-title">Tin tức</div>
+          <h1>Cập nhật thông tin mới nhất</h1>
+          <Paragraph gray71>
+            Vestibulum ornare ipsum sapien, vitae mollis augue fringilla sit
+            amet. Donec et mauris et arcu venenatis aliquam non ut dui.
+          </Paragraph>
+        </div>
+        <BlogList data={[...BLOGS].reverse()} />
+      </div>
+    </section>
+    <CallToAction />
   </main>
 );
 
